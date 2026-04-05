0f84603292ea5835bea7e383c9b551e9dda61b07
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2026-04-05 14:42:09 +0900

    feat : 청크 분할 모듈(splitter.py) 구현

--- test_manual.py
+++ test_manual.py
@@ -1,10 +1,17 @@
 from app.parsing.loader import collect_documents
+from langchain_core.documents import Document
+from app.parsing.splitter  import split_documents
+from app.parsing.filters import filter_chunks
 
-docs = collect_documents("tests/fixtures", "test_project", 2)
+docs = collect_documents("tests/fixtures", "test_project", 200)
 print(f"수집된 문서의 개수는?: {len(docs)}")
 
 # docs 파일 이름만 추출
 file_names = [doc.metadata["source"] for doc in docs]
 
 print(f"파일 이름 : {file_names}")
-print(f"문서 출력 : {docs}")
+
+chunks = split_documents(docs, chunk_size=100, chunk_overlap=20)
+chunks = filter_chunks(chunks)
+
+print(chunks)
